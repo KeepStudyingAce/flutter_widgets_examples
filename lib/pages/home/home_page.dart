1232ff5f046767bca5fd8ac9bf5f695955d7633d
Ace <15652913054@163.com>
2021-03-09 18:30:52 +0800

    首页添加搜索栏滚动效果：自定义flexible_space_bar组件（修改系统缩放动画）

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_widgets_example/common/app_config.dart';
 import 'package:flutter_widgets_example/common/common_style.dart';
 import 'package:flutter_widgets_example/pages/home/home/search_page.dart';
 import 'package:flutter_widgets_example/pages/home/home/swiper_page.dart';
 import 'package:flutter_widgets_example/routes/app_router.dart';
 import 'package:flutter_widgets_example/routes/navigation_utils.dart';
+import 'package:flutter_widgets_example/utils/common_util.dart';
+import 'package:flutter_widgets_example/widgets.dart/ace_flexible_space_bar.dart';
 import 'package:flutter_widgets_example/widgets.dart/common_appbar.dart';
 
 class HomePage extends StatefulWidget {
@@ -22,61 +25,51 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CommonAppBar(
-        title: "WidgetsList",
-        showLeading: true,
-        leading: Builder(builder: (context) {
-          //不包这层Builder调用openDrawer无效，猜测应该是context的原因
-          return IconButton(
-              icon: Icon(Icons.menu),
-              onPressed: () {
-                Scaffold.of(context).openDrawer();
-              });
-        }),
-        rightButtons: [
-          IconButton(
-              icon: Icon(Icons.search),
-              onPressed: () => showSearch(
-                  context: context, delegate: SearchBarDelegatePage()))
-        ],
-      ),
-      body: ListView(
-        children: [
-          _buildItem("AbsorbPointer & IgnorePointer",
-              () => AppRouter.goAbsorbIgnorePointPage(context)),
-          _buildItem(
-              "AnimationList", () => AppRouter.goAnimationListPage(context)),
-          _buildItem("ChipList", () => AppRouter.goChipPage(context)),
-          _buildItem(
-              "CustomClipper", () => AppRouter.goCustomChipperPage(context)),
-          _buildItem("CustomPaint", () => AppRouter.goCustomPaintPage(context)),
-          _buildItem(
-              "CustomRouteClipper", () => AppRouter.goCustomRoutePage(context)),
-          _buildItem("DismissibleList",
-              () => AppRouter.goDismissibleListPage(context)),
-          _buildItem(
-              "DraggableWidget", () => AppRouter.goDraggablePage(context)),
-          _buildItem("DragSortView", () => AppRouter.goDragSortPage(context)),
-          _buildItem(
-              "ExpansionList", () => AppRouter.goExpansionListPage(context)),
-          _buildItem(
-              "FrostedGlass", () => AppRouter.goFrostedGlassPage(context)),
-          _buildItem(
-              "FutureBuilder", () => AppRouter.goFutureBuildPage(context)),
-          _buildItem("InheritedWidget",
-              () => AppRouter.goInheritedWidgetPage(context)),
-          _buildItem(
-              "LayoutBuilder", () => AppRouter.goLayoutBuilderPage(context)),
-          _buildItem("Overlay", () => AppRouter.goOverlayPage(context)),
-          _buildItem("ReorderList", () => AppRouter.goReorderListPage(context)),
-          _buildItem(
-              "SliverAppBar", () => AppRouter.goSliverAppBarPage(context)),
-          _buildItem(
-              "StreamBuilder", () => AppRouter.goStreamBuilderPage(context)),
-          _buildItem("Swiper", () => AppRouter.goSwiper(context)),
-          _buildItem("Table", () => AppRouter.goTablePage(context)),
-          _buildItem("Transform", () => AppRouter.goTransformPage(context)),
-        ],
+      body: NestedScrollView(
+        headerSliverBuilder: (ctx, floated) {
+          return [
+            SliverAppBar(
+              actions: <Widget>[
+                IconButton(
+                    icon: Icon(Icons.search),
+                    onPressed: () => showSearch(
+                        context: context, delegate: SearchBarDelegatePage()))
+              ],
+              expandedHeight: AppConfig.kAppBarHeight +
+                  // AppConfig.iphoneXTopHeight(context) +
+                  50,
+              leading: Builder(builder: (context) {
+                //不包这层Builder调用openDrawer无效，猜测应该是context的原因
+                return IconButton(
+                    icon: Icon(Icons.menu),
+                    onPressed: () {
+                      Scaffold.of(context).openDrawer();
+                    });
+              }),
+              floating: false,
+              pinned: true,
+              flexibleSpace: AceFlexibleSpaceBar(
+                  titlePadding: EdgeInsets.zero,
+                  stretchModes: [],
+                  centerTitle: true,
+                  title: _buildSearchBar(),
+                  background: Container(
+                    margin: EdgeInsets.only(
+                        top: AppConfig.iphoneXTopHeight(context), bottom: 50),
+                    alignment: Alignment.center,
+                    child: Text(
+                      "WidgetLists",
+                      style: TextStyle(
+                          fontSize: 20, color: CommonColors.blackColor),
+                    ),
+                  )),
+            ),
+          ];
+        },
+        body: ListView(
+          padding: EdgeInsets.zero,
+          children: this._buildList(),
+        ),
       ),
       drawer: Drawer(
         child: ListView(
@@ -99,6 +92,65 @@ class _HomePageState extends State<HomePage> {
         ),
       ),
     );
+  }
+
+  Widget _buildSearchBar() {
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 300),
+      height: 40,
+      width: AppConfig.screenWidth(context) - 10,
+      margin: EdgeInsets.all(5),
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: CommonColors.separatorColor,
+          width: 0.5,
+        ),
+        color: CommonColors.background,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            Icons.search,
+            size: 20,
+          ),
+          Text(
+            "搜索",
+            style: TextStyle(fontSize: 12, color: CommonColors.blackColor),
+          ),
+        ],
+      ),
+    );
+  }
+
+  List<Widget> _buildList() {
+    return [
+      _buildItem("AbsorbPointer & IgnorePointer",
+          () => AppRouter.goAbsorbIgnorePointPage(context)),
+      _buildItem("AnimationList", () => AppRouter.goAnimationListPage(context)),
+      _buildItem("ChipList", () => AppRouter.goChipPage(context)),
+      _buildItem("CustomClipper", () => AppRouter.goCustomChipperPage(context)),
+      _buildItem("CustomPaint", () => AppRouter.goCustomPaintPage(context)),
+      _buildItem(
+          "CustomRouteClipper", () => AppRouter.goCustomRoutePage(context)),
+      _buildItem(
+          "DismissibleList", () => AppRouter.goDismissibleListPage(context)),
+      _buildItem("DraggableWidget", () => AppRouter.goDraggablePage(context)),
+      _buildItem("DragSortView", () => AppRouter.goDragSortPage(context)),
+      _buildItem("ExpansionList", () => AppRouter.goExpansionListPage(context)),
+      _buildItem("FrostedGlass", () => AppRouter.goFrostedGlassPage(context)),
+      _buildItem("FutureBuilder", () => AppRouter.goFutureBuildPage(context)),
+      _buildItem(
+          "InheritedWidget", () => AppRouter.goInheritedWidgetPage(context)),
+      _buildItem("LayoutBuilder", () => AppRouter.goLayoutBuilderPage(context)),
+      _buildItem("Overlay", () => AppRouter.goOverlayPage(context)),
+      _buildItem("ReorderList", () => AppRouter.goReorderListPage(context)),
+      _buildItem("SliverAppBar", () => AppRouter.goSliverAppBarPage(context)),
+      _buildItem("StreamBuilder", () => AppRouter.goStreamBuilderPage(context)),
+      _buildItem("Swiper", () => AppRouter.goSwiper(context)),
+      _buildItem("Table", () => AppRouter.goTablePage(context)),
+      _buildItem("Transform", () => AppRouter.goTransformPage(context)),
+    ];
   }
 
   Widget _buildItem(String title, Function onClick) {
